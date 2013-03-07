587d8a6aa8c0b2d7fd17163f903266de753b2cf6
Aaditya Shidham <aashidham@gmail.com>
2013-03-06 16:18:27 -0800

    Mykel's code

--- sketch_mar02a.ino
+++ sketch_mar02a.ino
@@ -1,120 +1,141 @@
+#include <Timers.h>
 #include <AFMotor.h>
 
-#define TURN_LEFT 0
-#define TURN_RIGHT 1
-#define GO_FORWARD 2
-#define GO_BACKWARD 3
 
-AF_DCMotor motor1(1);
-AF_DCMotor motor2(2);
-AF_DCMotor motor3(3);
-AF_DCMotor motor4(4);
-static int beaconLeft = 1;
-static int beaconRight = 2;
-static int tape = 3;
-static int state;
+#define SEARCH 0
+#define ATTACK 1
+#define SLIGHT_LEFT 2
+#define SLIGHT_RIGHT 3
+#define BACK_UP 4
+#define FAST 255
+#define SLOW 150
+#define ONE_SEC 1000
 
+AF_DCMotor motor1(1,MOTOR12_64KHZ);
+AF_DCMotor motor2(2,MOTOR12_64KHZ);
+AF_DCMotor motor3(3,MOTOR12_64KHZ);
+AF_DCMotor motor4(4,MOTOR12_64KHZ);
 
-
-//takes in between -255 and 255
-void leftSpeed(int speed)
-{
-  motor1.setSpeed(abs(speed));
-  motor2.setSpeed(abs(speed));
-  if(speed > 0)
-  {
-     motor1.run(FORWARD);
-     motor2.run(FORWARD);
-  }
-  else
-  {
-     motor1.run(BACKWARD);
-     motor2.run(BACKWARD);    
-  }
-}
-
-void rightSpeed(int speed)
-{
-  motor3.setSpeed(abs(speed));
-  motor4.setSpeed(abs(speed));
-   if(speed > 0)
-   {
-     motor3.run(FORWARD);
-     motor4.run(FORWARD);
-   } 
-   else
-   {
-     motor3.run(BACKWARD);
-     motor4.run(BACKWARD);
-   }
-}
-
-void turnRight()
-{
-  leftSpeed(255);
-  rightSpeed(-255);
-  state = TURN_RIGHT;
-}
-
-void turnLeft()
-{
-  leftSpeed(-255);
-  rightSpeed(255);
-  state = TURN_LEFT;
-}
-
-void goForward()
-{
-  leftSpeed(255);
-  rightSpeed(255);
-  state = GO_FORWARD;  
-}
-
-void goBackward()
-{
-  leftSpeed(-255);
-  rightSpeed(-255);
-  state = GO_BACKWARD;    
-}
-
-void respToTape()
-{
-  if(state == GO_FORWARD)
-  {
-    goBackward();
-  }
-  else goForward();
-}
+static int beaconLeft = 30;
+static int beaconRight = 34;
+static int tape = 38;
+static int state = SEARCH;
 
 void setup() {
   Serial.begin(9600);
   pinMode(beaconLeft,INPUT);
   pinMode(beaconRight,INPUT);
   pinMode(tape,INPUT);
-  goForward();
+  attack();
+  TMRArd_InitTimer(3,120*ONE_SEC);
 }
 
+void loop(){
+  if(TMRArd_IsTimerExpired(3) == TMRArd_EXPIRED){
+    leftSpeed(0);
+    rightSpeed(0);
+  }
+  else switch(state){ //switches between different states
+    case (SEARCH): //spinning left
+      if(tapeSensor()) backUp();
+      else if(bothBeacons()) attack();
+      else if(leftBeacon()) slightLeft();
+      else if(rightBeacon() || (TMRArd_IsTimerExpired(0) == TMRArd_EXPIRED)) slightRight();
+      break;
+    case (ATTACK): //full speed ahead
+      if(tapeSensor()) backUp();
+      else if(leftBeacon()) slightLeft();
+      else if(rightBeacon()) slightRight();
+      break;
+    case (SLIGHT_LEFT): //left motors slightly slower
+      if(tapeSensor())backUp();
+      else if(bothBeacons())attack();
+      else if(rightBeacon())slightRight();
+      break;
+    case (SLIGHT_RIGHT): //right motors slightly slower
+      if(tapeSensor())backUp();
+      else if(bothBeacons())attack();
+      else if(leftBeacon())slightLeft();
+      break;
+    case (BACK_UP): //full speed backward
+      if(tapeSensor() && (TMRArd_IsTimerExpired(2) == TMRArd_EXPIRED))slightRight();
+      else if(TMRArd_IsTimerExpired(1) == TMRArd_EXPIRED) search();
+      break;
+  }
+}
 
-void loop()
-{
-   if(HIGH == digitalRead(tape))
-   {
-      respToTape();
+//takes in between -255 and 255
+void leftSpeed(int speed){
+  motor1.setSpeed(abs(speed));
+  motor2.setSpeed(abs(speed));
+  if(speed > 0){
+     motor1.run(FORWARD);
+     motor2.run(FORWARD);
+  }
+  else{
+     motor1.run(BACKWARD);
+     motor2.run(BACKWARD);    
+  }
+}
+
+void rightSpeed(int speed){
+  motor3.setSpeed(abs(speed));
+  motor4.setSpeed(abs(speed));
+   if(speed > 0){
+     motor3.run(FORWARD);
+     motor4.run(FORWARD);
+   } 
+   else{
+     motor3.run(BACKWARD);
+     motor4.run(BACKWARD);
    }
-   else if(HIGH == digitalRead(beaconLeft) && HIGH == digitalRead(beaconRight))
-   {
-       goForward();
-   }
-   else if(HIGH == digitalRead(beaconLeft))
-   {
-      turnLeft();
-   }
-   else if(HIGH == digitalRead(beaconRight))
-   {
-       turnRight();
-   }
-   else //both beaconLeft and beaconRight are LOW
-   {
-      turnLeft(); 
-   }
+}
+
+void slightRight(){
+  leftSpeed(FAST);
+  rightSpeed(SLOW);
+  state = SLIGHT_RIGHT;
+} 
+
+void slightLeft(){
+  leftSpeed(SLOW);
+  rightSpeed(FAST);
+  state = SLIGHT_LEFT;
+}
+
+void search(){
+  leftSpeed(-175);
+  rightSpeed(175);
+  state = SEARCH;
+  TMRArd_InitTimer(0,6*ONE_SEC);
+}
+
+void attack(){
+  leftSpeed(FAST);
+  rightSpeed(FAST);
+  state = ATTACK;
+}
+
+void backUp(){
+  leftSpeed(-FAST);
+  rightSpeed(-FAST);  
+  state = BACK_UP;
+  TMRArd_InitTimer(1,1*ONE_SEC);
+  TMRArd_InitTimer(2,ONE_SEC/10);
+}
+
+int bothBeacons(){
+  return ((digitalRead(beaconLeft) == HIGH) && (digitalRead(beaconRight) ==  HIGH));
+}
+
+int leftBeacon(){
+  return ((digitalRead(beaconLeft) == HIGH) && (digitalRead(beaconRight) !=  HIGH));
+}
+
+int rightBeacon(){
+  return ((digitalRead(beaconLeft) != HIGH) && (digitalRead(beaconRight) ==  HIGH));
+}
+
+int tapeSensor(){
+  return (digitalRead(tape) == HIGH);
 }
